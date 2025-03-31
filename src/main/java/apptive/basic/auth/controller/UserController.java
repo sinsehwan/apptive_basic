0619338066b47e03108e348e90d8092dd39002be
sinsehwan <sk124590@gmail.com>
2025-03-31 21:25:01 +0900

    add : login, signup front page and controller

--- src/main/java/apptive/basic/auth/controller/UserController.java
+++ src/main/java/apptive/basic/auth/controller/UserController.java
@@ -3,8 +3,12 @@ package apptive.basic.auth.controller;
 import apptive.basic.auth.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -13,6 +17,36 @@ public class UserController {
     @GetMapping("/")
     public String loginHome(Model model){
         model.addAttribute("user", new User());
-        return "users/home";
+        return "/home";
+    }
+
+    @GetMapping("/signup")
+    public String signupForm(Model model){
+        model.addAttribute("user", new User());
+        return "/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute User user, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+        //검증 구현 필요
+
+
+        //성공 로직
+
+        return "redirect:/users/home";
+    }
+
+    @GetMapping("/login")
+    public String loginForm(Model model){
+        model.addAttribute(new User());
+
+        return "/login";
+    }
+
+    @PostMapping("login")
+    public String login(@ModelAttribute User user, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+        // 검증 로직
+
+        return "redirect:/main";
     }
 }
